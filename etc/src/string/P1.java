f18b4cf9870f44613a4f9a33a16c1233ee1c0535
carrotbat410 <carrotbat410@gmail.com>
2024-04-30 18:19:32 +0900

    String P1.

---
+++ etc/src/string/P1.java
@@ -0,0 +1,45 @@
+package string;
+
+import java.util.*;
+
+//문제명: 문자 찾기
+
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        //스캐너로 받을시
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        //직접 입력
+//        String str = "Computercooler";
+//        char c = 'c';
+
+        System.out.println(T.solution(str, c));
+    }
+
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == t) answer++;
+        }
+        return answer;
+    }
+
+    //향상된 반복문 이용한 풀이
+    public int solution2(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        for(char x : str.toCharArray()) {
+            if(x == t) answer++;
+        }
+        return answer;
+    }
+
+
+}
